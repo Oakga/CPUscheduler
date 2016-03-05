6d59d89e2b12f0f995593bbe188c46008f6e7d6c
Oakga <oakioakga@gmail.com>
2016-03-05 15:13:02 -0500

    Updated priority to job id
    while ((spot.next.priority != -1) &&  spot.next.priority >= newJob.priority){
    create setbehind node

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -8,12 +8,15 @@ public class PriorityQueue {
 	private PCB front;
 	private PCB back;
 	private PCB spot;
+	private PCB behindback;
 	
 	public PriorityQueue() {
 		back = new PCB();  // sentinel PCB;
 		front = new PCB(); 
+		front.next=back;
+		behindback=front;
 		back.priority = -1;
-		front.setNext(back);
+		
 	}
 	
 	/*
@@ -21,17 +24,25 @@ public class PriorityQueue {
 	 * priority is the priority of the PCB to be inserted;
 	 * 
 	 */
+	 
 	public void findSpot(PCB newJob){    
 		spot = front;
-		while ((spot.next.job_id != -1) &&  spot.next.priority >= newJob.priority){
+		while ((spot.next.priority != -1) &&  spot.next.priority >= newJob.priority){
 			spot = spot.next;
+			
 		}
+	}
+	
+	public void insertback(PCB newJob){
+		newJob.next=behindback.next;
+		behindback=newJob;
 	}
 	
 	public void insert(PCB newJob){
 		findSpot(newJob);
 		newJob.next = spot.next;
 		spot.next = newJob;
+		if (newJob.next==back)behindback=newJob;
 	}
 	
 	public PCB remove(PCB job){
