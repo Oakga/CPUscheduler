97451bc6131edffee7778eeba3933d1ca18b4459
Oakga <oakioakga@gmail.com>
2016-03-18 15:03:12 -0400

    Added files via upload

--- PCB.java
+++ PCB.java
@@ -1,30 +1,26 @@
 import java.util.Scanner;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 
-public class PCB {
-	public int time_in_BQ,Left_time_from_BQ,Left_time_from_RQ,Arrival_time_to_BQ,processing_time,Arrival_time_to_RQ,current_cpu_burst,job_id,num_cpu_burst,totalwait,priority,iocompletion;
+public class PCB implements Comparable<PCB>{
+	public int time_of_arrival,time_in_BQ,Left_time_from_BQ,Left_time_from_RQ,Arrival_time_to_BQ,processing_time,Arrival_time_to_RQ,current_cpu_burst,job_id,num_cpu_burst,totalwait,priority;
 	public String state;
 	public List<Integer> CPU_bursts; 
 	public PCB next;
-	public int counter;
 	
-	public PCB(int id,int amount_of_cpu_burst) throws FileNotFoundException{
+	
+	public PCB(int id,int amount_of_cpu_burst,int time_of_arrival) throws FileNotFoundException{
 		
 		this.job_id=id;
 		this.num_cpu_burst=amount_of_cpu_burst;
 		this.CPU_bursts =new ArrayList<Integer>(num_cpu_burst);
 		this.state="New"; 
 		this.current_cpu_burst=0;
-		//System.out.println(current_cpu_burst);
-		this.iocompletion=0;
 		this.next = null; 
-		this.counter=0;
-		
+		this.time_of_arrival=time_of_arrival;
+
 	}
 	
 	public PCB(){
@@ -43,7 +39,7 @@ public class PCB {
 	
 	public void printPCBbursts(){
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<(CPU_bursts.size()-1);i++){
+		for(int i=0;i<(CPU_bursts.size());i++){
 			sb.append(CPU_bursts.get(i)+" ");
 		}
 		System.out.println("This is whats in the node:"+sb);
@@ -55,14 +51,42 @@ public class PCB {
 	
 	
 	public void nextBurst(){
+		System.out.println("current_cpu_burst(before): "+current_cpu_burst);
+
+		if(current_cpu_burst<num_cpu_burst){
 		current_cpu_burst++;
-	}
+		}
+		
+		else{ 
+			System.out.println("Error here");
+			System.out.println("current_cpu_burst: "+current_cpu_burst);
+			System.out.println("num_cpu_burst: "+num_cpu_burst);
+		}
+		}
 
 	public boolean IsLastBurst() {
-		return (current_cpu_burst>=num_cpu_burst);
+		if(current_cpu_burst+1>=num_cpu_burst)return true;
+		else return false;
 	}
 	public int returnCurrent(){
-		System.out.println(CPU_bursts.get(current_cpu_burst));
 		return CPU_bursts.get(current_cpu_burst);
+	}
+	public int PCBid(){
+		return job_id;
+	}
+	private boolean equalTo(PCB node){
+		int i=this.CPU_bursts.get(this.current_cpu_burst);
+		System.out.println(this.job_id);
+		System.out.println(node.job_id);
+		System.out.println(i+" This current CPU burst comparing with "+node.CPU_bursts.get(node.current_cpu_burst));
+		
+		return (i==node.CPU_bursts.get(node.current_cpu_burst));
+	}
+	
+	public int compareTo(PCB node) {
+		int i=this.CPU_bursts.get(this.current_cpu_burst);
+		if(equalTo(node))return 0;
+		else if(i>node.CPU_bursts.get(current_cpu_burst)) return -1;
+		else return 1;
 	}
 }
