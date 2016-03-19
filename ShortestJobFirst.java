fc51aed390ef98bc5c615d91bbdf847660f39a1c
Oakga <oakioakga@gmail.com>
2016-03-19 00:35:27 -0400

    Update ShortestJobFirst.java

--- ShortestJobFirst.java
+++ ShortestJobFirst.java
@@ -1,62 +1,16 @@
-package nonRetardedCPUScheduler;
+package CPUScheduler;
 
-public class ShortestJobFirst implements Queue {
-	PCB front;
-	PCB back;
-	PCB sentinel;
-	PCB spot;
-	int size;
-	
-	ShortestJobFirst()
-	{
-		sentinel = null;
-		back.setNext(sentinel);
-		front.setNext(back);
-		size = 0;
-		
-	}
-	
-	@Override
-	public void insert(PCB job)
-	{
-		findspot(job);
-		job.setNext(spot.getNext());
-		spot.setNext(job);
-		++size;
-	}
+import java.util.Iterator;
+import java.util.PriorityQueue;
 
-	@Override
-	public void findspot(PCB newJob) {
-		spot = front;
-		while(spot.getNext() != sentinel && spot.getNext().cpuBursts[spot.getNext().currentBurst] < newJob.cpuBursts[newJob.currentBurst]){
-			spot = spot.getNext();
-		}	
-	}
-
-	@Override
-	public PCB remove() {
-		PCB temp = front;
-		front = front.getNext();
-		--size;
-		return temp;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return (size == 0);
-	}
-
-	@Override
-	public int size() {
-		return size;
-	}
-	
+public class ShortestJobFirst extends PriorityQueue<PCB> {
 	public void print(){
-		spot = front;
-		while(spot.getNext() != sentinel){
-			System.out.println("job id: " + spot.job_id + "burst: " + spot.cpuBursts[spot.currentBurst] + "\n");
-			spot = spot.getNext();
+		PCB current=null;
+		Iterator<PCB> iter=iterator();
+		while(iter.hasNext()){
+			current=iter.next();
+			System.out.print(current+" ");
+			System.out.println(" ");
 		}
 	}
-
 }
