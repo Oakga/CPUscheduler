df4b493847a55d8d2504069751e654df1d0992b3
Oakga <oakioakga@gmail.com>
2016-03-19 00:32:28 -0400

    Update PCB.java

--- PCB.java
+++ PCB.java
@@ -1,92 +1,91 @@
-import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
+package CPUScheduler;
 
 public class PCB implements Comparable<PCB>{
-	public int time_of_arrival,time_in_BQ,Left_time_from_BQ,Left_time_from_RQ,Arrival_time_to_BQ,processing_time,Arrival_time_to_RQ,current_cpu_burst,job_id,num_cpu_burst,totalwait,priority;
-	public String state;
-	public List<Integer> CPU_bursts; 
-	public PCB next;
-	
-	
-	public PCB(int id,int amount_of_cpu_burst,int time_of_arrival) throws FileNotFoundException{
-		
-		this.job_id=id;
-		this.num_cpu_burst=amount_of_cpu_burst;
-		this.CPU_bursts =new ArrayList<Integer>(num_cpu_burst);
-		this.state="New"; 
-		this.current_cpu_burst=0;
-		this.next = null; 
-		this.time_of_arrival=time_of_arrival;
-
-	}
-	
-	public PCB(){
-		this.next = null;
-	}
-	
-	/**********************methods*********************/
-	
-	public void putInArray(String bursts){
-		Scanner scan=new Scanner(bursts);
-		while(scan.hasNextInt()){
-			CPU_bursts.add(scan.nextInt());
-		}
-		scan.close();
-	}
-	
-	public void printPCBbursts(){
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<(CPU_bursts.size());i++){
-			sb.append(CPU_bursts.get(i)+" ");
-		}
-		System.out.println("This is whats in the node:"+sb);
-		}
-	
-	public void setNext(PCB next){
-		this.next=next;
-	}
-	
-	
-	public void nextBurst(){
-		System.out.println("current_cpu_burst(before): "+current_cpu_burst);
-
-		if(current_cpu_burst<num_cpu_burst){
-		current_cpu_burst++;
-		}
-		
-		else{ 
-			System.out.println("Error here");
-			System.out.println("current_cpu_burst: "+current_cpu_burst);
-			System.out.println("num_cpu_burst: "+num_cpu_burst);
-		}
-		}
-
-	public boolean IsLastBurst() {
-		if(current_cpu_burst+1>=num_cpu_burst)return true;
-		else return false;
-	}
-	public int returnCurrent(){
-		return CPU_bursts.get(current_cpu_burst);
-	}
-	public int PCBid(){
-		return job_id;
-	}
-	private boolean equalTo(PCB node){
-		int i=this.CPU_bursts.get(this.current_cpu_burst);
-		System.out.println(this.job_id);
-		System.out.println(node.job_id);
-		System.out.println(i+" This current CPU burst comparing with "+node.CPU_bursts.get(node.current_cpu_burst));
-		
-		return (i==node.CPU_bursts.get(node.current_cpu_burst));
-	}
-	
-	public int compareTo(PCB node) {
-		int i=this.CPU_bursts.get(this.current_cpu_burst);
-		if(equalTo(node))return 0;
-		else if(i>node.CPU_bursts.get(current_cpu_burst)) return -1;
+  public int job_id;
+  public String state;
+  public int burstCount;
+  public int[] cpuBursts;
+  public int currentBurst;
+  private PCB next;
+  public int priority;
+  public int ATRQ;
+  public int ATBQ;
+  public boolean virgin;
+  public int start;
+  
+  public int programCounter;    //this makes alot of other variables obsolete;
+  public int arrival;
+  public int completion;
+  public int processTime;
+  public int waitTime;
+  public int turnaround;
+  
+  public PCB()
+  {
+	  this.next = null;
+  }
+  
+  public PCB(int job_id, int arrival_time, int burstCount, String Bursts)
+  {
+	  this.job_id = job_id;
+	  this.burstCount = burstCount;
+	  this.arrival = arrival_time;
+	  this.cpuBursts = new int[burstCount];
+	  this.currentBurst = 0;
+	  this.state = "Ready";
+	  this.programCounter = 0;
+	  this.next = null;
+	  this.waitTime = arrival;
+	  this.processTime = 0;
+	  virgin = true;
+	  initBursts(Bursts);
+  };
+  
+  public PCB(String data){
+  }
+  public void initBursts(String Bursts)
+  {
+	  String[] array = Bursts.replaceAll("(^\\s+|\\s+$)", "").split("\\s+");
+	  
+	  for (int i=0; i<array.length; ++i) {
+		  this.cpuBursts[i] = Integer.parseInt(array[i]);
+		  
+	  };
+  };
+  
+  public void printBursts()
+  {	
+	  StringBuilder builder=new StringBuilder();
+	 for(int a: cpuBursts){
+		 System.out.print(a);
+		 System.out.print(" ");
+	 }
+	 System.out.println(" ");
+  }
+  
+  
+  public void setNext(PCB next)
+  {
+	  this.next = next;
+  }
+  
+  public PCB getNext()
+  {
+	  return next;
+  }
+  
+  public boolean lastBurst(){
+	  return (currentBurst == (burstCount-1));
+  }
+  
+  public void printStats(){
+	  System.out.println("Job Id: "+job_id+"|"+"Arrival Time: "+arrival+"|"+"Completion time: "+completion+"|"+"Processing time: "+processTime+"|"+"waiting time: "+waitTime+"|"+"turnaround time"+turnaround);
+  }
+  
+  
+  public int compareTo(PCB object) {
+		if(this.cpuBursts[(this.currentBurst)]==object.cpuBursts[object.currentBurst])return 0;
+		else if(this.cpuBursts[(this.currentBurst)]<object.cpuBursts[object.currentBurst]) return -1;
 		else return 1;
 	}
 }
